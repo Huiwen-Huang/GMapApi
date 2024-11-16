8bef9eca2da789d01025ed1a6672da1bb15430a0
Emma Huang <huiwenhuang1998@gmail.com>
2024-11-16 14:07:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <MapsApi />
+  <!-- Open the modal using ID.showModal() method -->
+  <button class="btn" onclick="my_modal_2.showModal()">open modal</button>
+  <dialog id="my_modal_2" class="modal">
+    <div class="modal-box">
+      <form method="dialog">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      </form>
+      <h3 class="text-lg font-bold">Hello!</h3>
+      <p class="py-4">Press ESC key or click outside to close</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <button>close</button>
+    </form>
+  </dialog>
+</template>
+
+<script>
+import MapsApi from './components/MapsApi.vue'
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+
+export default{
+  data() {
+    return {
+      text: 'Hello'
+
+    }
+  },
+  components: {
+    MapsApi,
+    BeakerIcon
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import  VueGoogleMaps from '@fawmi/vue-google-maps'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDnPsw3_O3qw5IcXCTP4bGCz1DziWK35LM',
+  },
+})
+
+app.mount('#app')
